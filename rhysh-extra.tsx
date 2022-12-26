61263de717091eec8beec4603fb0a2deb1c0d368
Maldrasen <maldrasen@gmail.com>
2022-12-26 09:42:27 -0600

    Add Houses feature map and update tilemaps

--- rhysh-extra.tsx
+++ rhysh-extra.tsx
@@ -6,200 +6,466 @@
  <image source="rhysh-extra.png" width="512" height="512"/>
  <tile id="0">
   <properties>
-   <property name="type" value="LargeStatue"/>
+   <property name="type" value="Hole"/>
   </properties>
  </tile>
  <tile id="1">
-  <properties>
-   <property name="facing" value="West"/>
-   <property name="type" value="Door"/>
-  </properties>
- </tile>
- <tile id="2">
-  <properties>
-   <property name="facing" value="North"/>
-   <property name="type" value="Door"/>
-  </properties>
- </tile>
- <tile id="3">
   <properties>
    <property name="direction" value="Up"/>
    <property name="type" value="Stairs"/>
   </properties>
  </tile>
- <tile id="4">
+ <tile id="2">
   <properties>
-   <property name="extent" value="ES"/>
-   <property name="type" value="Arches"/>
+   <property name="type" value="Pillar"/>
   </properties>
  </tile>
- <tile id="5">
+ <tile id="3">
   <properties>
-   <property name="extent" value="SW"/>
-   <property name="type" value="Arches"/>
-  </properties>
- </tile>
- <tile id="6">
-  <properties>
-   <property name="extent" value="SEW"/>
-   <property name="type" value="Arches"/>
-  </properties>
- </tile>
- <tile id="7">
-  <properties>
-   <property name="extent" value="NSW"/>
-   <property name="type" value="Arches"/>
-  </properties>
- </tile>
- <tile id="8">
-  <properties>
-   <property name="extent" value="NS"/>
-   <property name="type" value="Arches"/>
-  </properties>
- </tile>
- <tile id="9">
-  <properties>
-   <property name="extent" value="NESW"/>
-   <property name="type" value="Arches"/>
-  </properties>
- </tile>
- <tile id="10">
-  <properties>
-   <property name="type" value="LargePillar"/>
+   <property name="direction" value="NS"/>
+   <property name="type" value="Gate"/>
   </properties>
  </tile>
  <tile id="16">
   <properties>
-   <property name="type" value="Statue"/>
+   <property name="type" value="Point"/>
   </properties>
  </tile>
  <tile id="17">
-  <properties>
-   <property name="facing" value="East"/>
-   <property name="type" value="Door"/>
-  </properties>
- </tile>
- <tile id="18">
-  <properties>
-   <property name="facing" value="South"/>
-   <property name="type" value="Door"/>
-  </properties>
- </tile>
- <tile id="19">
   <properties>
    <property name="direction" value="Down"/>
    <property name="type" value="Stairs"/>
   </properties>
  </tile>
- <tile id="20">
+ <tile id="18">
   <properties>
-   <property name="extent" value="NE"/>
-   <property name="type" value="Arches"/>
+   <property name="type" value="Pit"/>
   </properties>
  </tile>
- <tile id="21">
+ <tile id="19">
   <properties>
-   <property name="extent" value="NW"/>
-   <property name="type" value="Arches"/>
-  </properties>
- </tile>
- <tile id="22">
-  <properties>
-   <property name="extent" value="NES"/>
-   <property name="type" value="Arches"/>
+   <property name="direction" value="EW"/>
+   <property name="type" value="Gate"/>
   </properties>
  </tile>
  <tile id="23">
   <properties>
    <property name="extent" value="NEW"/>
-   <property name="type" value="Arches"/>
+   <property name="type" value="Bars"/>
   </properties>
  </tile>
- <tile id="24">
+ <tile id="32">
   <properties>
-   <property name="extent" value="EW"/>
-   <property name="type" value="Arches"/>
+   <property name="facing" value="W"/>
+   <property name="type" value="SecretDoor"/>
   </properties>
  </tile>
- <tile id="25">
+ <tile id="33">
   <properties>
-   <property name="type" value="Pillar"/>
+   <property name="facing" value="N"/>
+   <property name="type" value="SecretDoor"/>
   </properties>
  </tile>
- <tile id="26">
+ <tile id="34">
   <properties>
-   <property name="type" value="DeepPit"/>
+   <property name="facing" value="W"/>
+   <property name="type" value="TrappedDoor"/>
   </properties>
  </tile>
- <tile id="36">
+ <tile id="35">
   <properties>
-   <property name="extent" value="ES"/>
-   <property name="type" value="Channel"/>
+   <property name="facing" value="N"/>
+   <property name="type" value="TrappedDoor"/>
   </properties>
  </tile>
- <tile id="37">
+ <tile id="48">
+  <properties>
+   <property name="facing" value="E"/>
+   <property name="type" value="SecretDoor"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="facing" value="S"/>
+   <property name="type" value="SecretDoor"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="facing" value="E"/>
+   <property name="type" value="TrappedDoor"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="facing" value="S"/>
+   <property name="type" value="TrappedDoor"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="facing" value="W"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="facing" value="N"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="facing" value="NW"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="69">
+  <properties>
+   <property name="facing" value="NE"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="facing" value="SEW"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="facing" value="NSW"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="facing" value="N"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="facing" value="NSEW"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="facing" value="E"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="facing" value="S"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="facing" value="SW"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="facing" value="SE"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="facing" value="NSE"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="87">
+  <properties>
+   <property name="facing" value="NEW"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="facing" value="N"/>
+   <property name="type" value="Door"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="extent" value="W"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="extent" value="N"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="extent" value="SE"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="101">
   <properties>
    <property name="extent" value="SW"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="38">
+ <tile id="102">
+  <properties>
+   <property name="extent" value="NSE"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="103">
   <properties>
    <property name="extent" value="SEW"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="39">
-  <properties>
-   <property name="extent" value="NSW"/>
-   <property name="type" value="Channel"/>
-  </properties>
- </tile>
- <tile id="40">
+ <tile id="104">
   <properties>
    <property name="extent" value="NS"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="41">
+ <tile id="105">
   <properties>
-   <property name="extent" value="NESW"/>
-   <property name="type" value="Channel"/>
+   <property name="extent" value="NSEW"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="52">
+ <tile id="114">
+  <properties>
+   <property name="extent" value="E"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="extent" value="S"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="116">
   <properties>
    <property name="extent" value="NE"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="53">
+ <tile id="117">
   <properties>
    <property name="extent" value="NW"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="54">
-  <properties>
-   <property name="extent" value="NES"/>
-   <property name="type" value="Channel"/>
-  </properties>
- </tile>
- <tile id="55">
+ <tile id="118">
   <properties>
    <property name="extent" value="NEW"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="56">
+ <tile id="119">
+  <properties>
+   <property name="extent" value="NSW"/>
+   <property name="type" value="Column"/>
+  </properties>
+ </tile>
+ <tile id="120">
   <properties>
    <property name="extent" value="EW"/>
-   <property name="type" value="Channel"/>
+   <property name="type" value="Column"/>
   </properties>
  </tile>
- <tile id="57">
+ <tile id="128">
   <properties>
-   <property name="type" value="Pit"/>
+   <property name="extent" value="E"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="130">
+  <properties>
+   <property name="extent" value="E"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="131">
+  <properties>
+   <property name="extent" value="S"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="132">
+  <properties>
+   <property name="extent" value="SE"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="133">
+  <properties>
+   <property name="extent" value="SW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="134">
+  <properties>
+   <property name="extent" value="SEW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="135">
+  <properties>
+   <property name="extent" value="NSW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="136">
+  <properties>
+   <property name="extent" value="NS"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="137">
+  <properties>
+   <property name="extent" value="NSEW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="extent" value="N"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="extent" value="W"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="extent" value="NE"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="149">
+  <properties>
+   <property name="extent" value="NW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="extent" value="NSE"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="extent" value="NEW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="152">
+  <properties>
+   <property name="extent" value="EW"/>
+   <property name="type" value="Fence"/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="extent" value="E"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="163">
+  <properties>
+   <property name="extent" value="S"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="extent" value="SE"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="extent" value="SW"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="166">
+  <properties>
+   <property name="extent" value="SEW"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="167">
+  <properties>
+   <property name="extent" value="NSW"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="168">
+  <properties>
+   <property name="extent" value="NS"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="169">
+  <properties>
+   <property name="extent" value="NSEW"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="extent" value="N"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="extent" value="W"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="180">
+  <properties>
+   <property name="extent" value="NE"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="extent" value="NW"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="182">
+  <properties>
+   <property name="extent" value="NSE"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="183">
+  <properties>
+   <property name="extent" value="NEW"/>
+   <property name="type" value="Barrier"/>
+  </properties>
+ </tile>
+ <tile id="184">
+  <properties>
+   <property name="extent" value="EW"/>
+   <property name="type" value="Barrier"/>
   </properties>
  </tile>
 </tileset>
