c878dc3e91122746b287739d686b43df76ab50c8
Maldrasen <maldrasen@gmail.com>
2022-12-29 09:48:03 -0600

    Working on a world map

---
+++ world/worldmap.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="worldmap" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="worldmap.png" width="1024" height="1024"/>
+ <tile id="19">
+  <properties>
+   <property name="type" value="Ocean"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="type" value="Mountain"/>
+  </properties>
+ </tile>
+</tileset>
